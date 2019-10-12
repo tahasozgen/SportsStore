403bd0c60a092104ba0525746520658592473df1
Taha Sözgen <sozgentaha70@gmail.com>
2019-10-13 00:13:37 +0300

    initial commit

---
+++ SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc2a16f8-0726-4363-af77-cdd9e71b7a7e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SportsStore.WebUI
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            /*
+            public static Route MapRoute(this RouteCollection routes, string name, string url, object defaults);
+
+            */
+            routes.MapRoute(
+                null,   //tstring name
+                "", //string url
+                new //object defaults
+                {
+                    controller = "Product",
+                    action = "List",
+                    category = (string)null,
+                    page = 1
+                }
+            );
+
+            /*
+            public static Route MapRoute(this RouteCollection routes, string name, string url, object defaults, object constraints);
+            */
+
+            routes.MapRoute(null, //this RouteCollection routes            
+                "Page{page}",   //string name            
+                new { controller = "Product", action = "List",  //string url
+                category = (string)null },// object defaults
+                new { page = @"\d+" }   //object constraints
+            );
+
+            /*
+            public static Route MapRoute(this RouteCollection routes, string name, string url, object defaults);
+            */
+            routes.MapRoute(
+                null,               // string name
+                "{category}",       //string url 
+                new { controller = "Product", action = "List", page = 1 }   //object defaults
+            );
+
+            /*
+            public static Route MapRoute(this RouteCollection routes, string name, string url, object defaults, object constraints);
+            */
+            routes.MapRoute(
+                null,          //string name  
+                "{category}/Page{page}",        //string url    
+                new { controller = "Product", action = "List" },      //object defaults      
+                new { page = @"\d+" }   // object constraints
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
+
+        }
+    }
+}
